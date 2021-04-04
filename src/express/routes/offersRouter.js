295a9fafec558c777571bef988de38e0204b8241
Alex Litvinenko <alexlit.double@gmail.com>
2021-04-05 00:38:00 +0700

    Шаблонизация

--- src/express/routes/offersRouter.js
+++ src/express/routes/offersRouter.js
@@ -5,11 +5,11 @@ const {URL} = require(`./../../service/constants`);
 const offersRouter = new Router();
 
 offersRouter.get(`${URL.OFFERSURLS.CATEGORY}/:id`, (request, response) =>
-  response.send(`${URL.OFFERS}${URL.OFFERSURLS.CATEGORY}/${Number.parseInt(request.params.id, 10)}`));
-offersRouter.get(URL.OFFERSURLS.ADD, (request, response) => response.send(`${URL.OFFERS}${URL.OFFERSURLS.ADD}`));
+  response.render(`category`));
+offersRouter.get(URL.OFFERSURLS.ADD, (request, response) => response.render(`new-ticket`));
 offersRouter.get(`${URL.OFFERSURLS.EDIT}/:id`, (request, response) =>
-  response.send(`${URL.OFFERS}${URL.OFFERSURLS.EDIT}/${Number.parseInt(request.params.id, 10)}`));
+  response.render(`ticket-edit`));
 
-offersRouter.get(URL.OFFERSURLS.ID, (request, response) => response.send(`${URL.OFFERS}/${Number.parseInt(request.params.id, 10)}`));
+offersRouter.get(URL.OFFERSURLS.ID, (request, response) => response.render(`ticket`));
 
 module.exports = offersRouter;
